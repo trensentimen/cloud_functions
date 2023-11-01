de59174b606b25f6427a4f3e108eddc83edee2f4
dani ferdinan <daniferdinandall@gmail.com>
2023-11-01 16:13:09 +0700

    add cf getAllTopic

---
+++ getAllTopic/function.go
@@ -0,0 +1,32 @@
+package getAllTopic
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	// be_trensentimen "github.com/trensentimen/be_trensen"
+	module "github.com/trensentimen/be_trensen/module"
+)
+
+func init() {
+	functions.HTTP("getAllTopic", TrensentimenGetAllTopic)
+}
+
+func TrensentimenGetAllTopic(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://trensentimen.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://trensentimen.github.io")
+	fmt.Fprintf(w, module.GCFHandlerGetAllTopic("MONGOSTRING", "trensentimen", "topic", r))
+	// fmt.Fprintf(w, be_trensentimen.GCFPostHandler("PASETOPRIVATEKEY", "MONGOSTRING", "trensentimen", "user", r))
+
+}
