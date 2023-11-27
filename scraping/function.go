d687136b993b3cbdf9f6a84b11e2f3da16425203
dani ferdinan <daniferdinandall@gmail.com>
2023-11-27 14:10:28 +0700

    add gcf scrapping

---
+++ scraping/function.go
@@ -0,0 +1,31 @@
+package scraping
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	// be_trensentimen "github.com/trensentimen/be_trensen"
+	module "github.com/trensentimen/be_trensen/module"
+)
+
+func init() {
+	functions.HTTP("scraping", TrensentimenGetAllTopic)
+}
+
+func TrensentimenGetAllTopic(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://trensentimen.my.id")
+		w.Header().Set("Access-Control-Allow-Origin", "https://trensentimen.my.id")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://trensentimen.my.id")
+	fmt.Fprintf(w, module.GCFHandlerScraping("PASETOPUBLICKEY", "MONGOSTRING", "trensentimen", r))
+}
